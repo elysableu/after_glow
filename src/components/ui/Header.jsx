cd85fcb5d6ef542992ba57933e091665ba4f24d5
elysableu <elysabward@gmail.com>
2026-05-18 15:12:48 -0600

    Add header component

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <div>
+            {/* <img /> <- add in logo img*/}
+            <h1>
+                Welcome to the games library!
+            </h1>
+        </div>
+    )
+}
