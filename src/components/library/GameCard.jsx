5a0999d9d8a2febbc28ece8f8f30f71f5f2f9e3c
elysableu <elysabward@gmail.com>
2026-05-21 16:03:58 -0600

    Add onClick hook

--- src/components/library/GameCard.jsx
+++ src/components/library/GameCard.jsx
@@ -1,10 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function GameCard({ game }) {
-    
+    const navigate = useNavigate()
+
     return (
         <div className="gameCard">
             { console.log("I'm here")}
             <h3>{game.name}</h3>
-            <button>Play!</button>
+            <button onClick={() => navigate(`/${game.id}`)}>Play!</button>
         </div>
     )
 }
