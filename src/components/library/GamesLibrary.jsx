f9d147e48fa46bb868586c516e08c6cde064896d
elysableu <elysabward@gmail.com>
2026-05-26 17:09:36 -0600

    Add base styles for GameLibrary

--- src/components/library/GamesLibrary.jsx
+++ src/components/library/GamesLibrary.jsx
@@ -1,9 +1,10 @@
 import { GAMES } from '../../lib/games'
 import GameCard from './GameCard'
+import styles from './GamesLibrary.module.css'
 
 export default function GamesLibrary() {
     return (
-        <div className="game-grid">
+        <div className={styles.libraryGrid}>
             {GAMES.map( game => 
                 <GameCard key={game.id} game={game} />
             )}
