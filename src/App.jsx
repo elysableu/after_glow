aad247666cab2284d9dd107bb99396d10b4b83a5
elysableu <elysabward@gmail.com>
2026-05-26 14:26:00 -0600

    Moved LiftScreen rendering to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
+import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LiftScreen from './components/layouts/LiftScreen'
 import Header from './components/ui/Header'
 import LibraryView from './components/library/LibraryView'
-import styles from './App.module.css'
 
 export default function App() {
+  const [ lifted, setLifted ] = useState(false);
+ 
   return (
     <>
-      <main>
-        <BrowserRouter>
+      <BrowserRouter>
+        <main>
+          <LiftScreen lifted={lifted} onStart={() => setLifted(true)} />
           <Header />
           <Routes>
             <Route path="/" element={<LibraryView />} />
           </Routes>
-        </BrowserRouter>
-      </main>
+        </main>
+      </BrowserRouter>
     </>
   )
 }
