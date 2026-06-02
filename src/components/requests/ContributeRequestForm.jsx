9f4339d2639d784c666fd26409db22ec774bebaa
elysableu <elysabward@gmail.com>
2026-06-02 15:32:32 -0600

    Created separate form components for game and contribution requests

--- src/components/requests/ContributeRequestForm.jsx
+++ src/components/requests/ContributeRequestForm.jsx
@@ -1,3 +1,48 @@
+import styles from './ContributeRequestForm.module.css'
+
 export default function ContributeRequestForm() {
-    return ()
+    return (
+        <div className={styles.contributeForm}>
+            {/* 
+                Information collected =>
+                    - Name
+                    - Email
+                    - GitHub Profile Link
+                    - Reason for joining/collborating 
+                    - Experience level => all welcome to allow me to monitor newer contributors
+                    - Publication alias/name
+                    - Optional additional message
+            */}
+            <form>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Name: </label>
+                    <input placeholder="Enter your name" id="name" className={styles.formInput} type="text"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Email: </label>
+                    <input placeholder="Enter your email" id="email" className={styles.formInput} type="email"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>GitHub Username: </label>
+                    <input placeholder="Enter your GitHub username" id="email" className={styles.formInput} type="email"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Reason for contributing: </label>
+                    <input placeholder="Enter your message" id="email" className={styles.formInput} type="email"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Skill Level: </label>
+                    <input placeholder="Enter your skill level" id="genre" className={styles.formInput} type="text"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Publication Alias: </label>
+                    <input placeholder="Enter your alias" id="game" className={styles.formInput} type="text"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Message: </label>
+                    <input placeholder="What else should I know" id="message" className={styles.formInput} type="text"/>
+                </div>
+            </form>
+        </div>
+    )
 }
--- src/components/requests/GameRequestForm.jsx
+++ src/components/requests/GameRequestForm.jsx
@@ -1,3 +1,39 @@
+import styles from './GameRequestForm.module.css'
+
 export default function GameRequestForm() {
-    return ()
+    return (
+         <div className={styles.gameForm}>
+            <form>
+                {/* 
+                    Information collected =>
+                        - Name
+                        - Email
+                        - Game Type
+                        - Game Name
+                        - Optional comment
+                */}
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Name: </label>
+                    <input placeholder="Enter your name" id="name" className={styles.formInput} type="text"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Email: </label>
+                    <input placeholder="Enter your email" id="email" className={styles.formInput} type="email"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Game Type: </label>
+                    <input placeholder="Enter the game type" id="genre" className={styles.formInput} type="text"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Game Name: </label>
+                    <input placeholder="Enter the name of the game" id="game" className={styles.formInput} type="text"/>
+                </div>
+                <div className={styles.formGroup}>
+                    <label className={styles.label}>Message: </label>
+                    <input placeholder="What else should I know" id="message" className={styles.formInput} type="text"/>
+                </div>
+                
+            </form>
+        </div>
+    )
 }
