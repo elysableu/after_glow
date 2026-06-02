f83703c0f2f32ec827b92638ebddd3c68d0046f6
elysableu <elysabward@gmail.com>
2026-06-02 16:22:08 -0600

    Update element orientation

--- src/components/requests/RequestView.jsx
+++ src/components/requests/RequestView.jsx
@@ -4,7 +4,6 @@ import styles from './RequestView.module.css'
 export default function RequestView() {
     return (
         <div className={styles.requestView}>
-            <h3>Make a request of our devs</h3>
             <RequestForm />
         </div>
     )
