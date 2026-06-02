71d85a04a6cbad2ef56f1335cf57e4093f57e35b
elysableu <elysabward@gmail.com>
2026-06-02 15:31:39 -0600

    Create requestView and form container

--- src/components/requests/RequestForm.jsx
+++ src/components/requests/RequestForm.jsx
@@ -1,3 +1,14 @@
+import ContributeRequestForm from './ContributeRequestForm'
+import GameRequestForm from './GameRequestForm'
+import RequestToggle from './RequestToggle'
+import styles from './RequestForm.module.css'
+
 export default function RequestForm() {
-    return ()
+    return (
+        <div className={styles.requestForm}>
+            <RequestToggle />
+            <ContributeRequestForm />
+            <GameRequestForm />
+        </div>
+    )
 }
--- src/components/requests/RequestView.jsx
+++ src/components/requests/RequestView.jsx
@@ -1,3 +1,11 @@
+import RequestForm from './RequestForm'
+import styles from './RequestView.module.css'
+
 export default function RequestView() {
-    return ()
+    return (
+        <div className={styles.requestView}>
+            <h3>Make a request of our devs</h3>
+            <RequestForm />
+        </div>
+    )
 }
